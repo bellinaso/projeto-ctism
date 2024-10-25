413ef32a3b91af0282f3caa94a049fc22b13eaa8
bellinaso <brunobellinaso2006@gmail.com>
2024-10-25 09:04:41 -0300

    Header modifications

--- view/index.php
+++ view/index.php
@@ -10,41 +10,46 @@
 
 <body>
     <header>
-        <div class="logo">
-            <a href="/view/index.php">
-                <img src="/public/images/logos_horizontal/logo1_white.svg" alt="" class="logo_image">
-            </a>
-        </div>
-        <div class="header_buttons">
-            <div class="reserve_page">
-                <a href="/view/establishments.php">
-                    RESERVAR AGORA
+        <div class="header">
+            <div class="logo">
+                <a href="/view/index.php">
+                    <img src="/public/images/logos_horizontal/logo1_white.svg" alt="" class="logo_image">
                 </a>
             </div>
-            <div class="account_pages">
-                <a href="/view/myaccount.php">
-                    <img class="profile_picture" src="/public/images/profile_picture.svg" alt="">
-                </a>
-                <div>
-                    <?php
-                        if(!isset($_SESSION['login'])) {
-                            echo '
-                                <span class="account_buttons">
-                                    <a href="/view/login.php">
-                                        ENTRE
-                                    </a>
-                                    <span>ou</span>
-                                </span>
-                                <span class="account_buttons">
-                                    <a href="/view/register.php">
-                                        CADASTRE-SE
-                                    </a>
-                                </span>
-                            ';
-                        }
-                    ?>
+            <div class="header_buttons">
+                <div class="reserve_page">
+                    <a href="/view/establishments.php">
+                        RESERVAR AGORA
+                    </a>
+                </div>
+                <div class="account_pages">
+                    <a href="/view/myaccount.php">
+                        <img class="profile_picture" src="/public/images/profile_picture.svg" alt="">
+                    </a>
+                    <div>
+                        <?php
+                            if(!isset($_SESSION['login'])) {
+                                echo '
+                                    <span class="account_buttons">
+                                        <a href="/view/login.php">
+                                            ENTRE
+                                        </a>
+                                        <span>ou</span>
+                                    </span>
+                                    <span class="account_buttons">
+                                        <a href="/view/register.php">
+                                            CADASTRE-SE
+                                        </a>
+                                    </span>
+                                ';
+                            }
+                        ?>
+                    </div>
                 </div>
             </div>
+        </div>
+        <div class="header_slogan">
+            <h1>Agendar<br> nunca foi<br> tão fácil!</h1>
         </div>
     </header>
     <main>
