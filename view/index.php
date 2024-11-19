4a3e707c8c2aa220eb813c1a9aff899d0b8174d2
Arthur Bernardo Paul <132100142+arthurbp08@users.noreply.github.com>
2024-11-19 07:47:38 -0300

    amigo

--- view/index.php
+++ view/index.php
@@ -14,8 +14,8 @@
 
 <body>
     <?php
-        @session_start();
-        $_SESSION['last_page'] = 'index.php';
+    @session_start();
+    $_SESSION['last_page'] = 'index.php';
     ?>
     <header>
         <div class="header">
@@ -41,10 +41,10 @@
                     </a>
                     <div>
                         <?php
-                            if(!isset($_SESSION['login'])) {
-                                @session_start();
-                                $_SESSION['last_page'] = 'myaccount.php';
-                                echo '
+                        if (!isset($_SESSION['login'])) {
+                            @session_start();
+                            $_SESSION['last_page'] = 'myaccount.php';
+                            echo '
                                     <span class="account_buttons">
                                         <a href="/view/login.php">
                                             ENTRE
@@ -57,14 +57,13 @@
                                         </a>
                                     </span>
                                 ';
-                            }
-                            else {
-                                echo '
+                        } else {
+                            echo '
                                     <a href="/view/myaccount.php">
                                         <span>MINHA CONTA</span>
                                     </a>
                                 ';
-                            }
+                        }
                         ?>
                     </div>
                 </div>
@@ -75,7 +74,7 @@
                 <h1>Agendar<br> nunca foi<br> tão fácil!</h1>
             </div>
             <div class="see_more red_button">
-                <a href="#about_us">    
+                <a href="#about_us">
                     Saiba mais
                 </a>
             </div>
@@ -84,47 +83,106 @@
             <i class="fa-solid fa-chevron-down"></i>
         </a>
     </header>
+  
     <main>
-        <div class="about_us" id="about_us">
-            <h1>Nossa missão</h1>
-            <div class="card_section">
-                <div class="card">
-                    <h2>Missão 1</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, architecto exercitationem repudiandae sequi iusto debitis porro et dolores nostrum consectetur impedit, possimus distinctio ducimus, harum hic accusantium. Nihil, molestias nisi!</p>
-                </div>
-                <div class="card">
-                    <h2>Missão 2</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, architecto exercitationem repudiandae sequi iusto debitis porro et dolores nostrum consectetur impedit, possimus distinctio ducimus, harum hic accusantium. Nihil, molestias nisi!</p>
-                </div>
-                <div class="card">
-                    <h2>Missão 3</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, architecto exercitationem repudiandae sequi iusto debitis porro et dolores nostrum consectetur impedit, possimus distinctio ducimus, harum hic accusantium. Nihil, molestias nisi!</p>
-                </div>
-                <div class="card">
-                    <h2>Missão 4</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, architecto exercitationem repudiandae sequi iusto debitis porro et dolores nostrum consectetur impedit, possimus distinctio ducimus, harum hic accusantium. Nihil, molestias nisi!</p>
-                </div>
+    <div class="about_us" id="about_us">
+        <!-- O que é o Bookease? -->
+        <div class="h1pri">
+            <h1>O que é o Bookease?</h1>
+        </div>
+        <div class="card_section">
+            <div class="largeCard">
+                <p>Bookease é uma plataforma intuitiva e inovadora que conecta clientes a diversos estabelecimentos, oferecendo soluções práticas e eficientes para agendamentos online. Com foco na simplificação do dia a dia, permite aos usuários reservar horários em serviços variados, como salões, clínicas, academias e outros, eliminando complicações e otimizando o tempo. Além de facilitar a organização de compromissos, o sistema melhora a experiência do cliente com uma interface amigável e personalizável, que atende tanto às necessidades dos usuários quanto dos estabelecimentos parceiros. Ao automatizar processos e modernizar o gerenciamento de reservas, o Bookease transforma a interação com serviços essenciais, promovendo conveniência e qualidade em cada etapa.</p>
             </div>
         </div>
-        <div class="about_us">
-            <h1>Quem nós somos?</h1>
-            <div class="card_section">
-                <div class="card">
-                    <img src="/public/images/profile_picture.svg" alt="" class="rounded_card_image">
-                    <h3>Arthur Bernardo Paul</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, architecto exercitationem repudiandae sequi iusto debitis porro et dolores nostrum consectetur impedit, possimus distinctio ducimus, harum hic accusantium. Nihil, molestias nisi!</p>
-                </div>
-                <div class="card">
-                    <img src="/public/images/profile_picture.svg" alt="" class="rounded_card_image">
-                    <h3>Bruno Bellinaso Brasil</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, architecto exercitationem repudiandae sequi iusto debitis porro et dolores nostrum consectetur impedit, possimus distinctio ducimus, harum hic accusantium. Nihil, molestias nisi!</p>
-                </div>
-            </div>
-        </div>
-    </main>
-    <footer>
 
-    </footer>
+        <!-- Nossa missão -->
+        <div class="h1sec">
+            <h1>Nossa missão</h1>
+        </div>
+        <div class="card_section">
+            <div class="card info">
+                <h2>Facilidade</h2>
+                <p>Tornar o agendamento simples e intuitivo, eliminando barreiras tecnológicas para todos os usuários.</p>
+            </div>
+            <div class="card info">
+                <h2>Acessibilidade</h2>
+                <p>Oferecer uma plataforma inclusiva, adaptável a diferentes dispositivos e necessidades específicas.</p>
+            </div>
+            <div class="card info">
+                <h2>Conexão</h2>
+                <p>Facilitar o contato entre clientes e estabelecimentos, promovendo parcerias duradouras.</p>
+            </div>
+            <div class="card info">
+                <h2>Inovação</h2>
+                <p>Trazer inovação para transformar a experiência de agendamento, com praticidade e segurança.</p>
+            </div>
+        </div>
+
+        <!-- Fundadores -->
+        <div class="about_us">
+            <h1>Fundadores</h1>
+            <div class="card_section">
+                <div class="card">
+                    <img src="/public/images/category_images/arthur.jpg" alt="" class="rounded_card_image">
+                    <h3>Arthur Bernardo Paul</h3>
+                    <p>Estudante do Colégio Técnico Industrial de Santa Maria (CTISM)</p>
+                </div>
+                <div class="card">
+                    <img src="/public/images/category_images/bruno.jpg" alt="Foto de Bruno Bellinaso Brasil" class="rounded_card_image">
+                    <h3>Bruno Bellinaso Brasil</h3>
+                    <p>Estudante do Colégio Técnico Industrial de Santa Maria (CTISM)</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+
+    <footer>
+    <div class="footer">
+        <div class="footer_top">
+          
+            <div class="footer_section">
+                <h4>Sobre o Bookease</h4>
+                <p>O Bookease é uma plataforma que simplifica agendamentos, conectando clientes e estabelecimentos com eficiência e praticidade.</p>
+            </div>
+
+         
+            <div class="footer_section">
+                <h4>Links Úteis</h4>
+                <ul>
+                    <li><a href="#about_us">Sobre Nós</a></li>
+                    <li><a href="/view/establishments.php">Reservar</a></li>
+                    <li><a href="/view/myaccount.php">Minha Conta</a></li>
+                    <li><a href="/view/contact.php">Contato</a></li>
+                </ul>
+            </div>
+
+           
+            <div class="footer_section">
+                <h4>Contato</h4>
+                <p>Email: <a href="mailto:support@bookease.com">support@bookease.com</a></p>
+                <p>Telefone: (55) 99727-0790</p>
+                <p>Endereço: Av Roraima, 1000 - Santa Maria, RS</p>
+            </div>
+        </div>
+
+       
+        <div class="footer_socials">
+            <a href="https://facebook.com" target="_blank"><i class="fab fa-facebook-square"></i></a>
+            <a href="https://instagram.com" target="_blank"><i class="fab fa-instagram"></i></a>
+            <a href="https://twitter.com" target="_blank"><i class="fab fa-twitter"></i></a>
+            <a href="https://linkedin.com" target="_blank"><i class="fab fa-linkedin"></i></a>
+        </div>
+
+        <!-- Copyright -->
+        <div class="footer_bottom">
+            <p>© 2024 Bookease. Todos os direitos reservados.</p>
+        </div>
+    </div>
+</footer>
+
+
 </body>
 
 </html>
